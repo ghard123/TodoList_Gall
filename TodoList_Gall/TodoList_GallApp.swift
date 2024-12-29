b423c8f40f3d86c1712a0ef0b0bec222b3234795
Greg <ghard123@gmail.com>
2024-12-30 10:22:26 +1100

    Initial Commit

---
+++ TodoList_Gall/TodoList_GallApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_GallApp.swift
+//  TodoList_Gall
+//
+//  Created by Greg on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_GallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
